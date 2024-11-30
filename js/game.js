01c4ae99ecb0d5aa7c62d3ee1d1ffc21cde8fea2
theresajvrensburg <theresa@isogramconsulting.com>
2024-11-30 13:48:48 +0100

    30 november 13:50

--- js/game.js
+++ js/game.js
@@ -2,6 +2,7 @@ class Game {
   constructor() {
     this.startScreen = document.getElementById("logo-background");
     this.gameScreen = document.getElementById("game-screen");
+    this.gameContainer = document.getElementById("game-container");
     this.gameEndScreen = document.getElementById("game-end");
     this.obstacles = [];
     this.width = 500;
@@ -29,6 +30,24 @@ class Game {
       "/images/chef_sml.png"
     );
 
+    this.token = new Token(
+      this.gameScreen,
+      200,
+      470,
+      24,
+      55,
+      "./images/fish.png"
+    );
+
+    this.door = new Door(
+      this.gameScreen,
+      100,
+      150,
+      100,
+      100,
+      "./images/door.png"
+    );
+
   }
 
   start() {
@@ -38,6 +57,7 @@ class Game {
 
     this.startScreen.style.display = "none";
     this.gameScreen.style.display = "block";
+    this.gameContainer.style.display = "flex";
 
     this.gameIntervalId = setInterval(() => {
       // console.log("starts game loop");
@@ -63,8 +83,9 @@ class Game {
 
     // Hide game screen
     this.gameScreen.style.display = "none";
+    this.gameContainer.style.display = "none";
     // Show end game screen
-    this.gameEndScreen.style.display = "block";
+    this.gameEndScreen.style.display = "flex";
   }
 
   update() {
@@ -75,7 +96,31 @@ class Game {
     for (let i = 0; i < this.obstacles.length; i++) {
       const obstacle = this.obstacles[i];
       obstacle.move();
-
+        // If the player's car collides with an obstacle
+        if (this.player.didCollide(obstacle)) {
+          // Remove the obstacle element from the DOM
+          obstacle.element.remove();
+          // Remove obstacle object from the array
+          this.obstacles.splice(i, 1);
+          // Reduce player's lives by 1
+          this.lives--;
+          // Update the counter variable to account for the removed obstacle
+          i--;
+        } // If the obstacle is off the screen (at the bottom)
+        else if (obstacle.top > this.height) {
+          // Increase the score by 1
+          this.score++;
+          document.getElementById("score").innerHTML = this.score;
+          // Remove the obstacle from the DOM
+          obstacle.element.remove();
+          // Remove obstacle object from the array
+          this.obstacles.splice(i, 1);
+          // Update the counter variable to account for the removed obstacle
+          i--;
+        }
+        document.getElementById("lives").innerHTML = this.lives;
+        document.getElementById("score").innerHTML = this.score;
+  
       
     }
 
